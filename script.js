e8abca9e1125401e73a67f7379f961c1ce0b2be8
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-10-02 23:03:10 -0500

    feat: Add button to clear grid

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let GRID_SIZE = 16;
 const container = document.querySelector('#container');
 const resizeBtn = document.querySelector('#resize-btn');
+const clearBtn = document.querySelector('#clear-btn');
 
 resizeBtn.addEventListener('click', function () {
   GRID_SIZE = Number(prompt('Enter number of pixels (max is 100):')); 
   removeGrid();
   drawGrid();
+})
+
+clearBtn.addEventListener('click', function () {
+  const rows = [...container.children];
+
+  rows.forEach(row => {
+    const squares = [...row.children];
+    squares.forEach(square => square.classList.remove('hovered'));
+  })
 })
 
 const createGridRow = function () {
